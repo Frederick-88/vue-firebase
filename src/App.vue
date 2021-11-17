ebdc04f1a61e4fe405f87c26dd506ffa9a1506c7
Chen Frederick <frederickfd88@gmail.com>
2021-11-17 21:58:36 +0700

    Updated Initial Vue Page Styling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
+    <img class="app-logo" src="https://i.ytimg.com/vi/Htt8AKeF1Kw/maxresdefault.jpg" alt="app-logo" />
+
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/todoapp">Todo App</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/customer-records"
+        >Customer Records with Image Hosting</router-link
+      >
     </div>
     <router-view />
   </div>
@@ -16,6 +20,12 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  .app-logo {
+    width: 200px;
+    position: absolute;
+    left: 0;
+  }
 }
 
 #nav {
