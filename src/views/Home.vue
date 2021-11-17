972f2cc297542d4032b41e3ea788325b8c752a2f
Chen Frederick <frederickfd88@gmail.com>
2021-11-17 21:56:05 +0700

    Updated Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home container">
-    <h4>My Vue App, Visit.</h4>
+    <h4>Hello I'm Chen Frederick, Welcome to My Vue.js + Firebase Fullstack Web Apps.</h4>
+    <h5 class="mt-4">There are 2 VueFire Fullstack App at the moment.</h5>
+    <h5 class="mt-2">Please kindly route to the pages via navigation bar at top.</h5>
   </div>
 </template>
 
